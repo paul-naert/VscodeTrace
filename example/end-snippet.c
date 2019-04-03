fc92765b07541509a79e98a24ad82d37d177ec4c
Paul <paul.naert@polymtl.ca>
2019-04-03 13:37:35 -0400

    cleanup

--- example/end-snippet.c
+++
@@ -1,9 +0,0 @@
-#include "minitrace.h"
-int long_running_thing_1;
-int long_running_thing_2;
-
-MTR_FINISH("background", "long_running", &long_running_thing_1);
-MTR_FINISH("background", "long_running", &long_running_thing_2);
-
-mtr_flush();
-mtr_shutdown();
--- example/start-snippet.c
+++
@@ -1,7 +0,0 @@
-#include "/home/pn/tests/clangd/clangd-vscode/example/minitrace.h"
-const signed char traceFile[] = "trace.json";
-
-mtr_init(traceFile);
-
-MTR_META_PROCESS_NAME("minitrace_test");
-MTR_META_THREAD_NAME("main thread");
